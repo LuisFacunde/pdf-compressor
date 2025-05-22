b76453de5a4becd98094750c27d47dae8aa11cac
Luis Henrique Facunde da Silva <luis.silva@FAV.COM.BR>
2025-05-22 11:58:34 -0300

    refactor: Criação da variável 'tamanho' para armazenamento da largura e altura da imagem original

--- codigo-fonte/imagens_pillow.py
+++ codigo-fonte/imagens_pillow.py
@@ -1,15 +1,13 @@
 import PIL
 from PIL import Image
 
-imagem = Image.open('c:/Users/luis.silva/Desktop/compactador-de-imagens/imagens-originais/trabalhador.jpg')
-
-width = imagem.width // 2
-height = imagem.height // 2
+imagem = Image.open('imagens-originais/trabalhador.jpg')
+tamanho = (imagem.width // 2, imagem.height // 2)
 
 print("Dimensões originais da imagem:", round(len(imagem.fp.read())/1024,2), "KB")
 
-imagem = imagem.resize((width, height), PIL.Image.NEAREST)
-imagem.save('c:/Users/luis.silva/Desktop/compactador-de-imagens/imagens-compactadas/trabalhador_50%.jpg')
+imagem = imagem.resize((tamanho), PIL.Image.NEAREST)
+imagem.save('imagens-compactadas/trabalhador_50%.jpg')
 
-imagem_compress = Image.open('c:/Users/luis.silva/Desktop/compactador-de-imagens/imagens-compactadas/trabalhador_50%.jpg')
+imagem_compress = Image.open('imagens-compactadas/trabalhador_50%.jpg')
 print("Dimensões da imagem redimensionada:", round(len(imagem_compress.fp.read())/1024,2), "KB")
